fa23e62cb8a0d44bba7845731c84f860b74a40ba
Nick <nick.camenisch@stud.hslu.ch>
2025-12-01 17:13:39 +0100

    🚧 Day two

---
+++ day_02/main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.part_one();
+        main.part_two();
+    }
+
+
+    private void part_one() {
+        // Implementation for part one
+    }
+
+
+    private void part_two() {
+        // Implementation for part two
+    }
+}

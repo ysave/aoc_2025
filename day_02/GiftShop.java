a68dba905a25fa9d161860864bca5e2bf3ed0a86
Nick <nick.camenisch@stud.hslu.ch>
2025-12-02 17:53:01 +0100

    ✨ Add solution Day 2 Part 2

--- day_02/GiftShop.java
+++ day_02/GiftShop.java
@@ -1,12 +1,14 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GiftShop {
 
     public static void main(String[] args) throws IOException {
-        Main main = new Main();
-        main.partOne();
+        GiftShop giftShop = new GiftShop();
+        giftShop.partTwo();
     }
 
     private void partOne() throws IOException {
@@ -35,7 +37,35 @@ public class GiftShop {
         System.out.println(sum);
     }
 
-    private void partTwo() {
-        // Implementation for part two
+    private void partTwo() throws IOException {
+        long sum = 0;
+        Set<Long> invalids = new HashSet<>();
+        String input = Files.readString(Path.of("day_02/inputs.txt")).trim();
+
+        for (String range : input.split(",")) {
+            String[] parts = range.split("-");
+            long start = Long.parseLong(parts[0]);
+            long end = Long.parseLong(parts[1]);
+
+            for (long n = start; n <= end; n++) {
+                String s = Long.toString(n);
+                int len = s.length();
+
+                for (int i = 1; i <= len / 2; i++) {
+                    if (len % i != 0) continue;
+
+                    String seq = s.substring(0, i);
+
+                    if (seq.repeat(len / i).equals(s)) {
+                        invalids.add(n);
+                        break;
+                    }
+                }
+            }
+        }
+        for (Long i : invalids) {
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }

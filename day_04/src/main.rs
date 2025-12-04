97211e869a983847fddb0680bfe8508215e87183
Nick <nick.camenisch@stud.hslu.ch>
2025-12-04 17:47:35 +0100

    ✨ Add solution for day 4 part 2

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
         grid.push(chars);
     }
     part1(&grid);
-    //part2(&grid);
+    part2(&mut grid);
 }
 
 fn part1(grid: &Vec<Vec<char>>) {
@@ -30,7 +30,7 @@ fn part1(grid: &Vec<Vec<char>>) {
             if current != '@' {
                 continue;
             }
-            let mut roll_count = 0;
+            let mut neighbors = 0;
 
             for (dr, dc) in dirs.iter() {
                 let new_r = r as isize + dr;
@@ -43,11 +43,11 @@ fn part1(grid: &Vec<Vec<char>>) {
                 if new_r >= grid.len() || new_c >= grid[0].len() { continue; } //check if in bounds
 
                 if grid[new_r][new_c] == '@' {
-                    roll_count += 1;
+                    neighbors += 1;
                 }
             }
 
-            if roll_count < 4 {
+            if neighbors < 4 {
                accessible_rolls += 1;
             }
         }   
@@ -55,9 +55,55 @@ fn part1(grid: &Vec<Vec<char>>) {
     println!("Solution Part One: {}", accessible_rolls);
 }
 
-/*
-fn part2(grid: &Vec<Vec<char>>) {
-    // Implementation for part 2
-}
 
-*/
+fn part2(grid: &mut Vec<Vec<char>>) {
+    let mut accessible_rolls = 0;
+    let mut accessible_roll_changes = true;
+    let dirs = [
+        (-1,-1), (-1,0), (-1,1),
+        (0,-1),          (0,1),
+        (1,-1),  (1,0),  (1,1),
+    ];
+
+    while accessible_roll_changes {
+        let rolls_before = accessible_rolls;
+        let mut to_remove = Vec::new();
+
+        for (r, row) in grid.iter().enumerate() {
+            for (c, _col) in row.iter().enumerate() {
+                if grid[r][c] != '@' { continue; }
+
+                let mut neighbors = 0;
+
+                for (dr, dc) in dirs {
+                    let new_r = r as isize + dr;
+                    let new_c = c as isize + dc;
+
+                    if new_r < 0 || new_c < 0 { continue; } //check if in bounds
+                    let (new_r,new_c) = (new_r as usize, new_c as usize);
+
+                    if new_r >= grid.len() || new_c >= grid[0].len() { continue; } //check if in bounds
+
+                    if grid[new_r][new_c] == '@' {
+                        neighbors += 1;
+                    }
+                }
+
+                if neighbors < 4 {
+                    accessible_rolls += 1;
+                    to_remove.push((r,c));
+                }
+            }
+        }
+
+        for (r,c) in to_remove {
+            grid[r][c] = '.';
+        }
+
+        if rolls_before == accessible_rolls {
+            accessible_roll_changes = false;
+        }
+    }
+
+    println!("Solution Part Two: {}", accessible_rolls);
+}
